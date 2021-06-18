2739f8ab6c3dd52b64bf25b3e96e81e4dced5fe7
Jorge Pastor <hyorch@gmail.com>
2021-06-18 13:14:07 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# MAVEN DEMO APP & JENKINS PIPELINE
+
+Maven app created with VCS
+
+Jenkinsfile. Jenkins declarative pipeline to compile and test Maven App
---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,19 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+   
+    public static void main( String[] args )
+    {        
+        int contador=0;
+        for(int i=0;i<5;i++){
+            contador = contador +i;
+            System.out.println(contador);
+        }
+        System.out.println( "Hello World!" );
+    }
+}
